2d6a58b4e1126cbc2e298e5c55fa3f5bce8027fd
NN <nnworkspace@gmail.com>
2018-11-13 12:12:43 +0100

    update build.gradle to resolve some dependencies

--- src/gen/java/org/ningning/crossword_gen/rest/WordpuzzleApiService.java
+++ src/gen/java/org/ningning/crossword_gen/rest/WordpuzzleApiService.java
@@ -7,7 +7,7 @@ import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 
 import org.ningning.crossword_gen.model.Error;
-import org.ningning.crossword_gen.model.PuzzleAndSolution;
+import org.ningning.crossword_gen.model.PuzzleAndSolutions;
 
 import java.util.List;
 
@@ -16,7 +16,7 @@ import java.io.InputStream;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSCXFCDIServerCodegen", date = "2018-11-12T16:51:24.304+01:00[Europe/Berlin]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSCXFCDIServerCodegen", date = "2018-11-13T11:58:02.617+01:00[Europe/Berlin]")
 public interface WordpuzzleApiService {
       public Response wordpuzzle(Integer rows, Integer cols, Float density, Integer shortestWordLength, SecurityContext securityContext);
 }
